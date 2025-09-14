6c9912700c2158aed4e31e7d9b2cc1d7f5a2dab8
Thato Mapheto <thatomapheto6@gmail.com>
2025-09-14 15:00:26 +0200

    Add basic_operations.py

---
+++ basic_operations.py
@@ -0,0 +1,8 @@
+number1 = 10
+number2 = 5
+sum = number2 + number1
+difference = number1 - number2
+product = number1 * number2
+print("Sum of 10 and 5 is", sum)
+print("Difference of 10 and 5 is", difference)
+print("Multiplication of 10 and 5 is", product)
