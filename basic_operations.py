bbe4bb09d87cd1a3bbcaf7fe6bc10be4f73cfb68
Thato Mapheto <thatomapheto6@gmail.com>
2025-09-14 15:14:14 +0200

    fix output wording to match requirements

--- basic_operations.py
+++ basic_operations.py
@@ -3,6 +3,6 @@ number2 = 5
 sum = number2 + number1
 difference = number1 - number2
 product = number1 * number2
-print("Sum of 10 and 5 is", sum)
-print("Difference of 10 and 5 is", difference)
+print("Addition of 10 and 5 is", sum)
+print("Subtraction of 10 and 5 is", difference)
 print("Multiplication of 10 and 5 is", product)
