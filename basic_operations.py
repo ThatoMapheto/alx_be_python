717e8e53475d45d8882f26242298c88af0f2000c
Thato Mapheto <thatomapheto6@gmail.com>
2025-09-14 20:19:01 +0200

    Add the simple interest calculation

--- basic_operations.py
+++ basic_operations.py
@@ -3,6 +3,6 @@ number2 = 5
 addition = number2 + number1
 subtraction = number1 - number2
 multiplication = number1 * number2
-print("Addition of number1 and number2 is", addition)
-print("Addition of number1 and number2 is", subtraction)
-print("Multiplication of number1 and number2 is", multiplication)
+print(f"Addition of {number1} and {number2} is {addition}")
+print(f"Subtraction of {number1} and {number2} is {subtraction}")
+print(f"Multiplication of {number1} and {number2} is {multiplication}")
